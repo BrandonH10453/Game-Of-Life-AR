7b094cc4f11f0f6f41803465a1fbeb60f7358a64
BrandonH10453 <Hernandez.brandon1950@gmail.com>
2024-03-13 20:54:24 -0500

    buttons

--- Game_of_Life_AR/Assets/Scripts/ObjectSpawner.cs
+++ Game_of_Life_AR/Assets/Scripts/ObjectSpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 
 [RequireComponent(typeof(ARRaycastManager))]
@@ -9,10 +10,13 @@ public class ObjectSpawner : MonoBehaviour
     [SerializeField] GameObject spawnablePrefab;
     [SerializeField] GameObject Board;
     [SerializeField] GameObject PlayerPiece;
+    [SerializeField] Button BoardButton;
+    [SerializeField] Button PlayerButton;
 
     List<ARRaycastHit> hits = new List<ARRaycastHit>();
     Camera arCamera;
     GameObject spawnedObject;
+    bool objectSpawned = false;
 
     void Start()
     {
@@ -23,33 +27,44 @@ public class ObjectSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.touchCount == 0)
-        return;
+        if (Input.touchCount == 0)
+            return;
 
         RaycastHit hit;
         Ray ray = arCamera.ScreenPointToRay(Input.GetTouch(0).position);
+        var hits = new List<ARRaycastHit>();
 
-        if(aRRaycastManager.Raycast(Input.GetTouch(0).position, hits))
+        if (aRRaycastManager.Raycast(Input.GetTouch(0).position, hits))
         {
-            if(Input.GetTouch(0).phase == TouchPhase.Began && spawnedObject == null)
+            if (Input.GetTouch(0).phase == TouchPhase.Began && spawnedObject == null)
             {
-                if(Physics.Raycast(ray, out hit))
+                if (Physics.Raycast(ray, out hit))
                 {
-                    if(hit.collider.gameObject.tag == "Spawnable")
+                    if (hit.collider.gameObject.tag == "Spawnable")
                     {
                         spawnedObject = hit.collider.gameObject;
                     }
                     else
                     {
-                        SpawnPrefab(hits[0].pose.position);
+                        if (spawnablePrefab != null && !objectSpawned)
+                        {
+                            SpawnPrefab(hits[0].pose.position);
+                            objectSpawned = true;
+
+                            // Hide the button once object is spawned
+                            if (spawnablePrefab == Board)
+                                BoardButton.gameObject.SetActive(false);
+                            else if (spawnablePrefab == PlayerPiece)
+                                PlayerButton.gameObject.SetActive(false);
+                        }
                     }
                 }
             }
-            else if(Input.GetTouch(0).phase == TouchPhase.Moved && spawnedObject != null)
+            else if (Input.GetTouch(0).phase == TouchPhase.Moved && spawnedObject != null)
             {
                 spawnedObject.transform.position = hits[0].pose.position;
             }
-            if(Input.GetTouch(0).phase == TouchPhase.Ended)
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
             {
                 spawnedObject = null;
             }
@@ -61,15 +76,19 @@ public class ObjectSpawner : MonoBehaviour
         spawnedObject = Instantiate(spawnablePrefab, spawnPosition, Quaternion.identity);
     }
 
-    // Method to set the spawnablePrefab to Golf Ball prefab
+    // Method to set the spawnablePrefab to Board prefab
     public void SetBoardPrefab()
     {
         spawnablePrefab = Board;
+        objectSpawned = false;
+        BoardButton.gameObject.SetActive(false);
     }
 
-    // Method to set the spawnablePrefab to Hole prefab
+    // Method to set the spawnablePrefab to PlayerPiece prefab
     public void SetPlayerPiecePrefab()
     {
         spawnablePrefab = PlayerPiece;
+        objectSpawned = false;
+        PlayerButton.gameObject.SetActive(false);
     }
 }
