0d3f4d5ef2d46e3fd6a5ac48bf30fe595e839bf5
BrandonH10453 <Hernandez.brandon1950@gmail.com>
2024-03-25 07:54:25 -0500

    changed from 7 to 10

--- Game_of_Life_AR/Assets/Scripts/RandomNumGenerator.cs
+++ Game_of_Life_AR/Assets/Scripts/RandomNumGenerator.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class RandomNumGenerator : MonoBehaviour
@@ -12,7 +9,7 @@ public class RandomNumGenerator : MonoBehaviour
     public TextMeshProUGUI randomNumberHolder;
     public void RandomGenerate()
     {
-        TheNumber = Random.Range(1, 7);
+        TheNumber = Random.Range(1, 10);
         //TextBox.GetComponent<Text>().text = "You rolled " + TheNumber;
         randomNumberHolder.text = "You rolled " + TheNumber.ToString();
     }
