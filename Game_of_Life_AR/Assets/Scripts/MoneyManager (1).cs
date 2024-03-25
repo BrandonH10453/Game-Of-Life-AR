5f997cfa7497b41a65f946ce4078c6a611ad1f4e
BrandonH10453 <Hernandez.brandon1950@gmail.com>
2024-03-25 10:40:07 -0500

    Careers

--- Game_of_Life_AR/Assets/Scripts/MoneyManager (1).cs
+++ Game_of_Life_AR/Assets/Scripts/MoneyManager (1).cs
@@ -12,10 +12,10 @@ public class MoneyManager : MonoBehaviour
     public Text careerText;
     Hashtable careerSalary = new Hashtable();
     Hashtable careerTaxes = new Hashtable();
-    string[] careerList = { "Astronaut", "Investor", "Dog Walker" };
+    string[] careerList = { "Doctor", "IT", "Teacher", "Accountant", "Police Officer", "Designer", "Artist", "Athlete", "Actor" };
 
     int money = 0;
-    string career = "Astronaut";
+    string career = "Doctor";
 
     private void Awake()
     {
@@ -71,71 +71,3 @@ public class MoneyManager : MonoBehaviour
         }
     }
 }
-
-
-/*************************************** Allen's Version******************************/
-
-/**
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class MoneyManager : MonoBehaviour
-{
-    public static MoneyManager instance;
-    public Text moneyText;
-    public Text careerText;
-    Hashtable careerSalary = new Hashtable();
-    Hashtable careerTaxes = new Hashtable();
-    string[] careerList = { "Astronaut", "Investor", "Dog Walker" };
-
-    int money = 0;
-    string career = "Astronaut";
-
-    private void Awake()
-    {
-        instance = this;
-    }
-
-    void Start()
-    {
-        moneyText.text = "$" + money.ToString();
-        careerSalary.Add("Astronaut", 100000);
-        careerSalary.Add("Investor", 90000);
-        careerSalary.Add("Dog Walker", 30000);
-        careerSalary.Add("Unemployed", 1);
-
-        careerTaxes.Add("Astronaut", 10000);
-        careerTaxes.Add("Investor", 9000);
-        careerTaxes.Add("Dog Walker", 3000);
-        careerTaxes.Add("Unemployed", 0);
-    }
-
-    public void AddMoneyTile()
-    {
-        money += Random.Range(5000, 30000);
-        moneyText.text = "$" + money.ToString();
-    }
-    
-    public void AddMoneyCareer()
-    {
-        money += (int)careerSalary[career];
-        moneyText.text = "$" + money.ToString();
-    }
-
-    public void changeCareerRandom()
-    {
-        career = careerList[Random.Range(0, careerList.Length)];
-        careerText.text = career.ToString();
-    }
-
-    public void taxes()
-    {
-        money -= (int)careerTaxes[career];
-        moneyText.text = "$" + money.ToString();
-    }
-}
-
-**/
-
