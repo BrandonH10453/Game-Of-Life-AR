84dd6ad2f194107a6c040a9918325eaf60a70c39
BrandonH10453 <Hernandez.brandon1950@gmail.com>
2024-03-25 12:43:52 -0500

    update MoneyManager

--- Game_of_Life_AR/Assets/Scripts/MoneyManager (1).cs
+++ Game_of_Life_AR/Assets/Scripts/MoneyManager (1).cs
@@ -1,5 +1,3 @@
-/**************************Muaadh's Version****************************/
-/**
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,79 +14,6 @@ public class MoneyManager : MonoBehaviour
 
     int money = 0;
     string career = "Doctor";
-
-    private void Awake()
-    {
-        instance = this;
-    }
-
-    void Start()
-    {
-        moneyText.text = "$" + money.ToString();
-        careerSalary.Add("Astronaut", 100000);
-        careerSalary.Add("Investor", 90000);
-        careerSalary.Add("Dog Walker", 30000);
-        careerSalary.Add("Unemployed", 1);
-
-        careerTaxes.Add("Astronaut", 10000);
-        careerTaxes.Add("Investor", 9000);
-        careerTaxes.Add("Dog Walker", 3000);
-        careerTaxes.Add("Unemployed", 0);
-    }
-
-    public void AddMoneyTile()
-    {
-        money += Random.Range(5000, 30000);
-        moneyText.text = "$" + money.ToString();
-    }
-
-    public void AddMoneyCareer()
-    {
-        money += (int)careerSalary[career];
-        moneyText.text = "$" + money.ToString();
-    }
-
-    public void changeCareerRandom()
-    {
-        career = careerList[Random.Range(0, careerList.Length)];
-        careerText.text = career.ToString();
-    }
-
-    public void taxes()
-    {
-        money -= (int)careerTaxes[career];
-        moneyText.text = "$" + money.ToString();
-    }
-
-    // This function will be called when the player collides with a tile
-    void OnCollisionEnter(Collision collision)
-    {
-        // Check if the collided object has the tag "Tile"
-        if (collision.gameObject.tag == "Spawnable")
-            else if (collision.gameObject.tag ==)
-        {
-            // Call the function you want to execute
-            AddMoneyTile();
-        }
-    }
-}
-**/
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class MoneyManager : MonoBehaviour
-{
-    public static MoneyManager instance;
-    public Text moneyText;
-    public Text careerText;
-    Hashtable careerSalary = new Hashtable();
-    Hashtable careerTaxes = new Hashtable();
-    string[] careerList = { "Astronaut", "Investor", "Dog Walker" };
-
-    int money = 0;
-    string career = "Astronaut";
 
     // Add a public GameObject for the confirmation button
     public GameObject confirmButton;
@@ -119,7 +44,7 @@ public class MoneyManager : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         // Check if the collided object has one of the tags
-        if (collision.gameObject.tag == "Collect" || collision.gameObject.tag == "Pay" || /* add the rest of your tags here */)
+        if (collision.gameObject.tag == "Collect" || collision.gameObject.tag == "Pay" /* add the rest of your tags here */)
         {
             // If the player collides with a tile, activate the confirmation button
             confirmButton.SetActive(true);
